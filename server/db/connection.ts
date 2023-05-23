1d943e3b3c9d3969f3db3c161fc7081491778edb
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-05-23 11:55:28 -0300

    Commented local DDBB.
    Commented local DDBB connection and environment variables.
    Fixed script "back" in "package.json" Server-Side

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,22 +1,16 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-import { /*  Dialect, Options, */ Sequelize } from 'sequelize'
+import { /* Dialect, */ Sequelize } from 'sequelize'
 
 /* const sequelizeOptions = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   dialect: process.env.DB_CONNECTION as Dialect,
   logging: false,
 }
 
-if (process.env.DB_CONNECTION === 'postgres') {
-  sequelizeOptions.dialectModule = require('pg')
-}
-
 export const sequelize = new Sequelize(
-  `${process.env.DB_DATABASE}`,
-  `${process.env.DB_USERNAME}`,
-  `${process.env.DB_PASSWORD}`,
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASS}`,
   sequelizeOptions
 ) */
 
